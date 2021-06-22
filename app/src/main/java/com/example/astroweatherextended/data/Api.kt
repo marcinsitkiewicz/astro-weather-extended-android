7daca94351f28c5ddcb0b9108b299e68ed934342
Marcin Sitkiewicz <sitkiewicz.m.2f@gmail.com>
2021-06-22 16:24:12 +0200

    Added Api interface for endpoints

---
+++ app/src/main/java/com/example/astroweatherextended/data/Api.kt
@@ -0,0 +1,16 @@
+package com.example.astroweatherextended.data
+
+import com.example.astroweatherextended.data.models.OpenWeatherData
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface Api {
+    @GET("data/2.5/onecall")
+    fun getWeather(
+        @Query("lat") lat: String,
+        @Query("long") long: String,
+        @Query("exclude") exclude: String,
+        @Query("appid") appid: String
+    ): Call<OpenWeatherData>
+}
